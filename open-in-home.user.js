1fd5d18f4fbec95891b5316c3e8f2b605ffb078f
Julian Vos <post@julianvos.nl>
2023-12-20 14:08:52 +0100

    Update open-in-home.user.js

--- open-in-home.user.js
+++ open-in-home.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Open in home instance
 // @namespace    https://julianvos.nl
-// @version      1.0
+// @version      1.1
 // @description  Open a Lemmy instance in your home instance with a one click
 // @author       Julian Vos
 // @match        https://*/c/*
+// @run-at       document-idle
 // @grant        GM.setValue
 // @grant        GM.getValue
 // ==/UserScript==
@@ -22,45 +23,36 @@
 
     const currentInstance = window.location.hostname;
 
-    window.addEventListener('load', (e) => {
-        if (currentInstance === homeInstance) {
-            console.debug('Already on home instance');
-            return;
-        }
+    if (currentInstance === homeInstance) {
+        console.debug('Already on home instance');
+        return;
+    }
 
-        // Get the community name from the path
-        const communityRegex = /\/c\/(\w+)/;
-        const regexResult = communityRegex.exec(window.location.pathname);
+    // Get the community name from the path
+    const communityRegex = /\/c\/(\w+)/;
+    const regexResult = communityRegex.exec(window.location.pathname);
 
-        // Regex has 1 capture group, so result should always have 2 entries
-        if (regexResult.length < 2) {
-            console.debug('Failed to find community name in path');
-            return;
-        }
+    // Regex has 1 capture group, so result should always have 2 entries
+    if (regexResult.length < 2) {
+        console.debug('Failed to find community name in path');
+        return;
+    }
 
-        const community = regexResult[1];
+    const community = regexResult[1];
 
-        console.debug('Finding community link');
+    console.debug('Finding community link');
+    const sidebarTitle = document.querySelector('#sidebarContainer #sidebarMain .card-body div');
 
-        // kinda assuming the first element is always the community link at the top of the page
-        // not sure if that's true though
-        const communityLink = document.getElementsByClassName('community-link')[0];
+    const openInHomeButton = document.createElement('button');
 
-        const openInHomeButton = document.createElement('button');
+    openInHomeButton.classList = 'btn d-block mb-2 w-100 btn-secondary';
 
-        openInHomeButton.classList.add('btn');
-        openInHomeButton.classList.add('m-2');
-        openInHomeButton.classList.add('btn-secondary');
+    openInHomeButton.onclick = () => {
+        window.open(`https://${homeInstance}/c/${community}@${currentInstance}`);
+    };
 
-        openInHomeButton.onclick = () => {
-            window.open(`https://${homeInstance}/c/${community}@${currentInstance}`);
-            console.log(`https://${homeInstance}/c/${community}@${currentInstance}`);
-        };
+    openInHomeButton.innerText = 'Open in home instance';
 
-        openInHomeButton.innerText = 'Open in home instance';
-
-        console.debug('Inserting button');
-
-        communityLink.after(openInHomeButton);
-    });
+    console.debug('Inserting button');
+    sidebarTitle.after(openInHomeButton);
 })();
